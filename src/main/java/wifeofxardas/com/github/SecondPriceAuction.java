b8de52884f91a4280c6840d80ce2ea299f671b55
Evgenii Solntsev <thexardass@gmail.com>
2018-06-08 12:23:12 +0300

    test contract

--- src/main/java/wifeofxardas/com/github/SecondPriceAuction.java
+++ src/main/java/wifeofxardas/com/github/SecondPriceAuction.java
@@ -3,26 +3,43 @@ package wifeofxardas.com.github;
 import org.neo.smartcontract.framework.SmartContract;
 import org.neo.smartcontract.framework.Helper;
 import org.neo.smartcontract.framework.services.neo.Storage;
+import org.neo.smartcontract.framework.services.neo.Runtime;
 
 public class SecondPriceAuction extends org.neo.smartcontract.framework.SmartContract {
 
     public static byte[] Main(String operation, Object[] args){
+        Runtime.log(operation);
+
+        if(args.length == 1) Runtime.log((String) args[0]);
+        if(args.length == 2) Runtime.log((String) args[1]);
+
         if (operation.equals("openLot")) {
-
+            return SecondPriceAuction.openLot();
         } else if (operation.equals("cancelLot")) {
-
+            return SecondPriceAuction.cancelLot();
         } else if (operation.equals("payTo")) {
-
+            return SecondPriceAuction.payTo();
         } else if (operation.equals("confirmPay")) {
-
+            return SecondPriceAuction.confirmPay();
         }
 
         Storage.put(Storage.currentContext(), "Greeting to the World", "Hello World!");
         return Storage.get(Storage.currentContext(),"Greeting to the World");
     }
 
-    public String openLot () {
-        return "a";
+    public static byte[] openLot () {
+        return "a".getBytes();
     }
 
+    public static byte[] cancelLot () {
+        return "a".getBytes();
+    }
+
+    public static byte[] payTo () {
+        return "a".getBytes();
+    }
+
+    public static byte[] confirmPay () {
+        return "a".getBytes();
+    }
 }
