b536873cce2784e6febe68e840e31009524d26d1
Evgenii Solntsev <thexardass@gmail.com>
2018-06-08 12:37:08 +0300

    delete echos

--- build.sh
+++ build.sh
@@ -4,10 +4,9 @@ currentDir=`/bin/pwd`
 className=SecondPriceAuction
 fileName=$className.class
 classFile=./target/classes/wifeofxardas/com/github/$fileName
-echo $currentDir
+
 cp $classFile ../neo-compiler/neoj
 cd ../neo-compiler/neoj
 dotnet run $fileName
 
-echo $className.avm
 cp ./$className.avm $currentDir
