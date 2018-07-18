31dbaec907fa2b332cf9880cc92a053b6a28f861
Evgenii Solntsev <thexardass@gmail.com>
2018-07-18 15:57:36 +0300

    add license

--- src/main/java/wifeofxardas/com/github/Utils.java
+++
@@ -1,9 +0,0 @@
-package wifeofxardas.com.github;
-
-import org.neo.smartcontract.framework.Helper;
-
-public class Utils {
-    public static String stringConcat (String a, String b) {
-        return Helper.asString(Helper.concat(Helper.asByteArray(a), Helper.asByteArray(b)));
-    }
-}
